e740f5ee5ee2d36117cd44f0bd45881e63bda258
UktamovAkbar <slavok2612@gmail.com>
2023-02-08 18:37:49 +0500

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        
-        <img src="./logoxozm.png" alt=""  width="100px"/>
-       <input type="text" className='footer__search'/>
-        
+    <div className="footer">
+      <img src="./logoxozm.png" alt="" width="100px" />
+      <input type="text" className="footer__search" />
+      
+        <h1 className="footer__number">+998(91)44222317</h1>
+      <p className=""></p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
