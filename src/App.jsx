77a94199ca28811ec4f0bde830dc9b75b749531c
UktamovAkbar <slavok2612@gmail.com>
2023-02-09 21:32:54 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import {Routes, Route } from 'react-router-dom' 
-import Layout from './components/Layout'
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-        <Route path='layout' element={<Layout/>}/>
-    </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
