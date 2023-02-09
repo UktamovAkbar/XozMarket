2e34e5f1af6e0c290a9f59052962bc9d825faf2f
UktamovAkbar <slavok2612@gmail.com>
2023-02-09 20:14:32 +0500

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="" style="height: 482px;">
+    <footer className="footer">
       <img src="./logoxozm.png" alt="" width="100px" />
       <input type="text" className="footer__search" />
+      <a href="tel:+998(91)44222317">+998(91)44222317</a>
+      <p className="footer__subtitle">Интернет-магазин товаров для дома.</p>
       
-        <h1 className="footer__number">+998(91)44222317</h1>
-      <p className=""></p>
+    </footer>
     </div>
   );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,9 @@ import Footer from './Footer'
 
 const Layout = () => {
   return (
-    <>
+    <div>
         <Footer />
-    </>
+    </div>
   )
 }
 
