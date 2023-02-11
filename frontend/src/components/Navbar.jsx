9d49618b68438e10bb3e5e0c7279b1b32bd994c9
Amir <shodiyevamir999@gmail.com>
2023-02-11 07:51:26 +0500

    edded frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/Navbar.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserGroup,
+  faStar,
+  faMagnifyingGlass,
+  faBasketShopping,
+} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+function Navbar() {
+    return (
+        <div>
+           <h1>hello</h1>
+        </div>
+    );
+}
+export default Navbar;
